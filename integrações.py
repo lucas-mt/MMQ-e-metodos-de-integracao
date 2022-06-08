acb78a3656ffa74414e6ee7b96d61b3ee86f667d
Lucas Mt <lucas.mdlira@gmail.com>
2022-06-08 10:23:55 -0300

    comentário sobre n

--- integrações.py
+++ integrações.py
@@ -15,6 +15,7 @@ class MetInt:
             sm += (self.y[i]+self.y[i+1])
         return (self.h/2)*sm
     def s13(self):
+        #n deve ser divisível por 2
         aux1, aux2 = 0, 0
         for i in range(1, len(self.y)-1):
             if i%2==0:
@@ -23,6 +24,7 @@ class MetInt:
                 aux2+=self.y[i]
         return (self.h/3)*(self.y[0] + 2*aux1 + 4*aux2 + self.y[-1])
     def s38(self):
+        #n deve ser divisível por 3
         aux1, aux2 = 0, 0
         for i in range(1, len(self.y)-1):
             if i%3!=0:
--- área principal_integrações.py
+++ área principal_integrações.py
@@ -3,6 +3,6 @@ from integrações import *
 fc = input('função: ')
 linf = float(input('limite inferior: '))
 lsup = float(input('limite superior: '))
-n = mult(3)
+n = mult(1)
 
 itg = MetInt(fc, linf, lsup, n)
